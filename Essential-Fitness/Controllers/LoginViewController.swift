7fdda256465027334bcf6c36781d5eb258ab1919
HeshanCoder <102891254+HeshanCoder@users.noreply.github.com>
2023-05-05 05:21:31 -0700

    add signing , create new user, and forgot password button and register events for those buttons

--- Essential-Fitness/Controllers/LoginViewController.swift
+++ Essential-Fitness/Controllers/LoginViewController.swift
@@ -15,11 +15,32 @@ class LoginViewController: UIViewController {
     private let usernameField = CustomTextField(fieldType: .username)
     private let passwordField = CustomTextField(fieldType: .password)
     
+    private let signInButton = CustomButton(title: "Sign In", hasBackground: true, fontSize: .big)
+    private let newUserButton = CustomButton(title: "New user? Create Account", fontSize: .med)
+    private let forgotPasswordButton = CustomButton(title: "Forgot Password?", fontSize: .small)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
 
         self.setUpUI()
+        
+        // Registering events. (Target: is the button itself, action: an action to happend. exampale a method, for : UI event)
+        self.signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        self.newUserButton.addTarget(self, action: #selector(didTapNewUser), for: .touchUpInside)
+        self.forgotPasswordButton.addTarget(self, action: #selector(didTapForgotPassword), for: .touchUpInside)
+    }
+    
+    @objc func didTapSignIn(){
+        print("DEBUG PRINT:", "didTapSignIn")
+    }
+    
+    @objc func didTapNewUser(){
+        print("DEBUG PRINT:", "didTapNewUser")
+    }
+    
+    @objc func didTapForgotPassword(){
+        print("DEBUG PRINT:", "didTapNewUser")
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -31,10 +52,16 @@ class LoginViewController: UIViewController {
         self.view.addSubview(headerView)
         self.view.addSubview(usernameField)
         self.view.addSubview(passwordField)
+        self.view.addSubview(signInButton)
+        self.view.addSubview(newUserButton)
+        self.view.addSubview(forgotPasswordButton)
         
         headerView.translatesAutoresizingMaskIntoConstraints = false
         usernameField.translatesAutoresizingMaskIntoConstraints = false
         passwordField.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        newUserButton.translatesAutoresizingMaskIntoConstraints = false
+        forgotPasswordButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             // layoutMarginsGuide will prevent app screen go over status bar
@@ -49,9 +76,24 @@ class LoginViewController: UIViewController {
             self.usernameField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier:  0.85),
             
             self.passwordField.topAnchor.constraint(equalTo: self.usernameField.bottomAnchor, constant: 22),
-            self.passwordField.centerXAnchor.constraint(equalTo: self.usernameField.centerXAnchor),
+            self.passwordField.centerXAnchor.constraint(equalTo: self.headerView.centerXAnchor),
             self.passwordField.heightAnchor.constraint(equalToConstant: 55),
-            self.passwordField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier:  0.85)
+            self.passwordField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier:  0.85),
+            
+            self.signInButton.topAnchor.constraint(equalTo: self.passwordField.bottomAnchor, constant: 22),
+            self.signInButton.centerXAnchor.constraint(equalTo: self.headerView.centerXAnchor),
+            self.signInButton.heightAnchor.constraint(equalToConstant: 55),
+            self.signInButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier:  0.85),
+            
+            self.newUserButton.topAnchor.constraint(equalTo: self.signInButton.bottomAnchor, constant: 11),
+            self.newUserButton.centerXAnchor.constraint(equalTo: self.headerView.centerXAnchor),
+            self.newUserButton.heightAnchor.constraint(equalToConstant: 44),
+            self.newUserButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier:  0.85),
+            
+            self.forgotPasswordButton.topAnchor.constraint(equalTo: self.newUserButton.bottomAnchor, constant: 6    ),
+            self.forgotPasswordButton.centerXAnchor.constraint(equalTo: self.headerView.centerXAnchor),
+            self.forgotPasswordButton.heightAnchor.constraint(equalToConstant: 44),
+            self.forgotPasswordButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier:  0.85)
             
         ])
         
